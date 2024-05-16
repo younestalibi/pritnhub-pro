0d70fe7dfbef1a636a8253c4613ca5b991a1b5fa
younes talibi <younessetalibi11@gmail.com>
2024-05-16 22:32:32 +0100

    update auth and the error handeling and detail product page

--- printhub-server/controllers/AuthController.js
+++ printhub-server/controllers/AuthController.js
@@ -8,7 +8,6 @@ const { validationResult } = require("express-validator");
 exports.register = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors.array());
     return res.status(422).json({ error: errors.array() });
   }
   try {
@@ -34,7 +33,6 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors.array());
     return res.status(422).json({ error: errors.array() });
   }
   try {
